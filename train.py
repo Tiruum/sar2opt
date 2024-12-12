a6a0f2a1aaf296a5b4d4b26a2cc2e1bccf0e1793
Tiruum <selintimur@yandex.ru>
2024-12-12 20:10:55 +0300

    Added VGG16 PerceptionLoss to PatchGAN + recombinated PatchGAN hyperparameters

--- train.py
+++ train.py
@@ -3,6 +3,12 @@ from torch import nn
 from torch.optim import Adam
 from tqdm import tqdm
 import os
+from torchvision.models import vgg16, VGG16_Weights
+
+def perceptual_loss(generated, real, model):
+    gen_features = model(generated)
+    real_features = model(real)
+    return nn.L1Loss()(gen_features, real_features)
 
 project_root = os.path.abspath(os.path.join(os.getcwd()))
 
@@ -139,8 +145,8 @@ def train_gan(train_loader, generator, discriminator, g_optimizer, d_optimizer, 
             g_output = generator(sar)
 
             # Реальные метки и поддельные метки
-            real_labels = torch.ones_like(discriminator(optical), device=device)
-            fake_labels = torch.zeros_like(discriminator(g_output.detach()), device=device)
+            real_labels = torch.ones_like(discriminator(optical), device=device) * 0.9
+            fake_labels = torch.zeros_like(discriminator(g_output.detach()), device=device) + 0.1
 
             # Потери дискриминатора для реальных и поддельных данных
             d_real = discriminator(optical)
@@ -161,7 +167,8 @@ def train_gan(train_loader, generator, discriminator, g_optimizer, d_optimizer, 
             d_fake = discriminator(g_output)
             g_adv_loss = criterion_gan(d_fake, real_labels)
             g_l1_loss = criterion_l1(g_output, optical)
-            g_loss = g_adv_loss + 100 * g_l1_loss  # Коэффициент L1 Loss
+            g_perceptual_loss = perceptual_loss(g_output, optical, perceptual_model)
+            g_loss = g_adv_loss + 10 * g_l1_loss + 5 * g_perceptual_loss
 
             g_optimizer.zero_grad()
             g_loss.backward()
@@ -175,7 +182,9 @@ def train_gan(train_loader, generator, discriminator, g_optimizer, d_optimizer, 
         d_losses.append(d_loss_total / len(train_loader))
         save_losses(g_losses, d_losses, f"{project_root}/logs")
 
-        tqdm.write(f"Epoch: [{epoch+1}/{epochs}] Generator Loss: {g_loss_total/len(train_loader):.4f}, Discriminator Loss: {d_loss_total/len(train_loader):.4f}")
+        val_loss = validate_gan(test_loader, generator, criterion_l1, DEVICE, epoch, epochs)
+
+        tqdm.write(f"Epoch: [{epoch+1}/{epochs}] Generator Loss: [train: {g_loss_total/len(train_loader):.4f}, val: {val_loss:.4f}], Discriminator Loss: {d_loss_total/len(train_loader):.4f}")
 
         # Сохранение лучших моделей
         if g_loss_total < best_g_loss:
@@ -186,7 +195,6 @@ def train_gan(train_loader, generator, discriminator, g_optimizer, d_optimizer, 
             best_d_loss = d_loss_total
 
         # Проверка ранней остановки
-        val_loss = validate_gan(test_loader, generator, criterion_l1, DEVICE, epoch, epochs)
         early_stopping(val_loss)  # Передаём валидационную потерю генератора
         if early_stopping.early_stop:
             print(f"Early stopping triggered after {epoch + 1} epochs.")
@@ -203,12 +211,14 @@ if __name__ == "__main__":
     SAVE_MODEL_PATH = f"{project_root}/saved_models"  # Папка для сохранения модели
     os.makedirs(SAVE_MODEL_PATH, exist_ok=True)
 
+    perceptual_model = vgg16(weights=VGG16_Weights.IMAGENET1K_V1).features[:9].to(DEVICE).eval()
+
     # Инициализация модели, функции потерь и оптимизатора
     generator = UNet(in_channels=1, output_channels=3).to(DEVICE)
     discriminator = PatchDiscriminator(in_channels=3).to(DEVICE)
 
     # Инициализация оптимизаторов
-    g_optimizer = Adam(generator.parameters(), lr=LEARNING_RATE)
+    g_optimizer = Adam(generator.parameters(), lr=2e-4, betas=(0.5, 0.999))
     d_optimizer = Adam(discriminator.parameters(), lr=LEARNING_RATE)
 
     criterion_gan = nn.BCELoss()
